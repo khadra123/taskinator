22a9a752c785df8d7a578ed863d6e3303d490c5a
Khadra Mohamed <khadra.m123@gmail.com>
2021-04-04 17:19:26 -0400

    added form functionality to page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,11 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var createTaskHandler = function (){
+
+    //This prevents the browser from constantly refreshing
+    event.preventDefault();
+
     var listItemEl = document.createElement("li")
     listItemEl.className ="task-item";
     listItemEl.textContent ="This is a new task.";
@@ -9,8 +13,8 @@ var createTaskHandler = function (){
 
 }
 
-//when button clicked create task
-buttonEl.addEventListener("click", createTaskHandler);
+//waits for the form to be submitted by using the button and then creating the task
+formEl.addEventListener("submit", createTaskHandler);
 
 
 
