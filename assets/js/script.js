1bd86408a2fea98a14862b2ed9feb76f9a7e9a59
Khadra Mohamed <khadra.m123@gmail.com>
2021-04-04 18:05:00 -0400

    refactored code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,58 @@
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function (){
+var taskFormHandler = function(event) {
 
-    //This prevents the browser from constantly refreshing
-    event.preventDefault();
+  //This prevents the browser from constantly refreshing
+  event.preventDefault();
+  var taskNameInput = document.querySelector("input[name='task-name'").value;
+  var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
-    var listItemEl = document.createElement("li")
-    listItemEl.className ="task-item";
-    listItemEl.textContent ="This is a new task.";
-    tasksToDoEl.appendChild(listItemEl);
+  // check if inputs are empty (validate)
+  if (taskNameInput === "" || taskTypeInput === "") {
+    alert("You need to fill out the task form!");
+    return false;
+  }
+  
+  formEl.reset();
 
-}
+  // reset form fields for next task to be entered
+  document.querySelector("input[name='task-name']").value = "";
+  document.querySelector("select[name='task-type']").selectedIndex = 0;
 
-//waits for the form to be submitted by using the button and then creating the task
-formEl.addEventListener("submit", createTaskHandler);
+  //packaged the data as an obj to be caleed in the createTaskEl()
+  var taskDataObj = {
+    name: taskNameInput,
+    type: taskTypeInput
+  };
+
+  //send obj as argument to createTaskEl
+  createTaskEl(taskDataObj);
+};
+
+//This will hold the code to creat the new task HTML element
+//This also sets an obj(taskDataObj) as the argument as we can call on the obj properties  
+var createTaskEl = function(taskDataObj) {
+  // create list item
+  var listItemEl = document.createElement("li");
+  listItemEl.className = "task-item";
+
+  // create div to hold task info and add to list item
+  var taskInfoEl = document.createElement("div");
+  //give it a class name
+  taskInfoEl.className = "task-info";
+  //add HTML content to the div
+  //also gets the info of the obj as well
+  taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
+  listItemEl.appendChild(taskInfoEl);
+
+  console.dir(listItemEl);
+
+  // add list item to list
+  tasksToDoEl.appendChild(listItemEl);
+};
+
+formEl.addEventListener("submit", taskFormHandler);
 
 
 
